abd06574ed567b0afd8fedb13cf5cd41421ea1f9
Chris H.J. Hartgerink <c.hartgerink@gmail.com>
2014-03-03 21:47:26 +0100

    Did some work on effect sizes, meta-analysis of pilot study

--- Analyzing/a.Functions/ESComp.R
+++ Analyzing/a.Functions/ESComp.R
@@ -3,37 +3,94 @@ ESComp <- function(# Function to compute effect sizes for a \code{statcheck} obj
   ### \code{statcheck} object
   adjusted=TRUE
   ### Logical for giving adjusted effect sizes
-){
+  ){
   esComp <- ifelse(x$test_statistic=="t",
-                   (x$test_statistic_value^2*(1 / x$df1)) / (((x$test_statistic_value^2*1) / x$df1) + 1),
-                   ifelse(
-                     x$test_statistic=="F",
-                     (x$test_statistic_value*(x$df1 / x$df2)) / (((x$test_statistic_value*x$df1) / x$df2) + 1),
-                     ifelse(
-                       x$test_statistic=="r",
-                       x$test_statistic_value[x$test_statistic=="r"]^2,
+   (x$test_statistic_value^2*(1 / x$df1)) / (((x$test_statistic_value^2*1) / x$df1) + 1),
+   ifelse(
+     x$test_statistic=="F",
+     (x$test_statistic_value*(x$df1 / x$df2)) / (((x$test_statistic_value*x$df1) / x$df2) + 1),
+     ifelse(
+       x$test_statistic=="r",
+       x$test_statistic_value[x$test_statistic=="r"]^2,
                        #                        ifelse(
                        #                          x$test_statistic=="Chi2",
                        #                          sqrt(x$test_statistic_value[x$test_statistic=="Chi2"]/(x$df1[x$test_statistic=="Chi2"]+1)),
                        NA
-                     )
-                   )
-  )
+                       )
+     )
+   )
   #   )
-  adjESComp <- ifelse(x$test_statistic=="t",
-                      (x$test_statistic_value^2 * (1 / x$df1) - (1 / x$df1)) / (((x$test_statistic_value^2*1) / x$df1) + 1),
-                      ifelse(
-                        x$test_statistic=="F",
-                        (x$test_statistic_value * (x$df1 / x$df2) - (x$df1 / x$df2)) / (((x$test_statistic_value*x$df1) / x$df2) + 1),
-                        ifelse(
-                          x$test_statistic=="r",
+adjESComp <- ifelse(x$test_statistic=="t",
+  (x$test_statistic_value^2 * (1 / x$df1) - (1 / x$df1)) / (((x$test_statistic_value^2*1) / x$df1) + 1),
+  ifelse(
+    x$test_statistic=="F",
+    (x$test_statistic_value * (x$df1 / x$df2) - (x$df1 / x$df2)) / (((x$test_statistic_value*x$df1) / x$df2) + 1),
+    ifelse(
+      x$test_statistic=="r",
                           x$test_statistic_value[x$test_statistic=="r"]^2,# Need to refine for adjusted effect size
                           #                        ifelse(
                           #                          x$test_statistic=="Chi2",
                           #                          sqrt(x$test_statistic_value[x$test_statistic=="Chi2"]/(x$df1[x$test_statistic=="Chi2"]+1)),
                           NA
-                        )
-                      )
+                          )
+    )
   )
-  return(cbind(esComp,adjESComp))
+return(cbind(esComp,adjESComp))
+}
+
+
+r2z <- function(# Transform correlation coefficients into Fisher's Z coefficients
+  ### Transforming correlation (r) coefficients into Fisher's Z coefficients for testing.
+  r
+  ### The (vector) of correlation coefficient(s)
+  ){
+  ##seealso<<
+  ### \code{z2r}
+  return(.5*log((1+r)/(1-r)))
+}
+
+z2r <- function(# Transform Fisher's Z coefficients into correlation coefficients
+  ### Transforming Fisher's Z coefficients into correlations coefficients to get backtransformed results after testing.
+  z
+  ### The (vector) of Fisher Z coefficient(s)
+  ){
+  ##seealso<<
+  ### \code{z2r}
+  return((exp(2*z)-1)/(exp(2*z)+1))
+}
+
+nCalc <- function(# Compute sample sizes based on dfs.
+  x
+### A \code{statcheck} object.
+){
+  testN <- ifelse(x$test_statistic=="t",
+   x$df1+1,
+   ifelse(
+     x$test_statistic=="F",
+     x$df2-(x$df1+1),
+     ifelse(
+       x$test_statistic=="r",
+       x$df+1,
+                       #                        ifelse(
+                       #                          x$test_statistic=="Chi2",
+                       #                          sqrt(x$test_statistic_value[x$test_statistic=="Chi2"]/(x$df1[x$test_statistic=="Chi2"]+1)),
+                       NA
+                       )
+     )
+   )
+  #   )
+return(testN)
+}
+
+sVarComp <- function(# Compute sampling variances for correlation coefficients
+  ### Use this function to compute the sampling variances for a meta-analysis based on correlation coefficient effect sizes. 
+  ### See the "See Also" section for the functions to use the resulting sampling variance in accordance with
+  x
+  ### A \code{statcheck} object
+  ){
+  ##seealso<<
+  ##\link{ESComp}, \link{r2z}, \link{z2r}
+  testN <- nCalc(x)
+  sVar <- 1/(testN-3)
+  return(sVar)
 }
--- mastercode.R
+++ mastercode.R
@@ -24,17 +24,63 @@ copilot$test_statistic_value <- suppressWarnings(as.numeric(sub(",",".",copilot$
 copilot$df1 <- suppressWarnings(as.numeric(sub(",",".",copilot$df1)))
 copilot$df2 <- suppressWarnings(as.numeric(sub(",",".",copilot$df2)))
 
-## Computing fisher test statistics (inexact)
+# Computing fisher test statistics (inexact)
 resPilot <- FisherExTest(copilot$p_value_computed, copilot$pap_id)
-
-## Computing unadjusted and adjusted effect sizes
-copilot <- cbind(copilot, ESComp(copilot))
-
 # Descriptive statistics
 # Non-significant results
 # Percent
 mean(resPilot$PercentNonSig)
-hist(resPilot$PercentNonSig,breaks=20)
+hist(resPilot$PercentNonSig,breaks=20, main="Percentages of non-significant results", xlab="Percentage")
+# Number of statistics reported
+mean(resPilot$CountNSig+resPilot$CountSig)
+sd(resPilot$CountNSig+resPilot$CountSig)
+
+mean(resPilot$CountNSig)
+sd(resPilot$CountNSig)
+
+mean(resPilot$CountSig)
+sd(resPilot$CountSig)
+
+# Results
+fishTestSign <- round(na.omit(cbind(resPilot$Fish[resPilot$PFish<.1], resPilot$PFish[resPilot$PFish<.1])),3)
+# Number of signif results
+dim(fishTestSign)[1]
+fishTestComplSign <- round(na.omit(cbind(resPilot$FishCompl[resPilot$PFishCompl<.1], resPilot$PFishCompl[resPilot$PFishCompl<.1])),3)
+# Number of signif results complement
+dim(fishTestComplSign)[1]
+
+
+# Running meta-analysis on unadjusted effect sizes
+if(!require(metafor)){install.packages('metafor')}
+# Computing unadjusted and adjusted effect sizes
+copilot <- cbind(copilot, ESComp(copilot))
+# Converting effect sizes into r
+rSqES <- sqrt(copilot$esComp)
+# Fisher's r to Z transformation for M-A
+rES <- r2z(rSqES)
+# Calculating sampling variance
+sVar <- sVarComp(copilot)
+# Selecting out the negative sampling variances
+# Caused by either very small samples or by df1 > df2 in F tests
+# See next line of commented out code for the cases that have this
+# (na.omit(copilot[((sVar)<0),3:5]))
+rES <- rES[!(sVar<0)]
+sVar <- sVar[!sVar<0]
+# Random effects MA
+# Note: this takes a long time to run (under DerSimonian-Laird approx. 5-6minutes, REML takes >1h).
+# That is why I comment this out.
+# x1 <- proc.time()[1]
+# modelPilot <- rma(yi=rES, vi=sVar, method="DL", digits=10)
+# x2 <- proc.time()[1]
+# x2-x1
+# Transforming back MA results
+estPilot <- z2r(modelPilot$b[1])
+sePilot <- z2r(modelPilot$se)
+ciLowPilot <- z2r(modelPilot$ci.lb)
+ciHighPilot <- z2r(modelPilot$ci.ub)
+tau2Pilot <- z2r(modelPilot$tau2)
+resModelPilot <- round(cbind(estPilot, sePilot, ciLowPilot, ciHighPilot, tau2Pilot),3)
+
 
 
 ###########
