f2dabd73e82d908d9a3d7f9982b818d23b268c85
CHJ <c.hartgerink@gmail.com>
2015-07-11 08:42:15 +0200

    Drafted analyses for gender effect, corrected one coding (which did not contain enough info but accidentally did get final coding)

--- gender/kappa.R
+++ gender/kappa.R
@@ -14,11 +14,13 @@ cohen.kappa(gend[, 6:8])
 # 2 = effect expected
 # 3 = no expectation
 table(gend$significance, gend$final_code)
-
+options(scipen = 5)
 for(sig in unique(gend$significance)){
   for(code in unique(gend$final_code[!is.na(gend$final_code)])){
     sel <- gend$significance == sig & gend$final_code == code
   
-    print(FisherMethod(x = gend$Computed[sel], id = 1))
+    x <- (FisherMethod(x = gend$Computed[sel], id = 1, alpha = 0))
+    
+    cat(sprintf("For %s %s, k = %s, chi2 = %s, p = %s\n", sig, code, x$CountNSig, x$Fish, x$PFish))
   }
 }
